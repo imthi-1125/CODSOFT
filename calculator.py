9772364ec109df22b2c2c5b2aab403b45ed9979f
imthiyas basha E <imthiyas2511@gmail.com>
2025-01-17 23:36:49 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,31 +1,40 @@
+import math
+
 while True:
-    num1=int(input("Enter a number 1:"))
-    num2=int(input("Enter a number 2:"))
-    add=num1+num2
-    sub=num1-num2
-    multi=num1*num2
-    div=num1/num2 if num2!=0 else print("undefined by zero")
-    operation=input("Enter your operation[add/sub/multi/div]:").lower()
-    if (operation=="add"):
-        print("addition of numbers:",add)
-    elif(operation=="sub"):
-        print("substraction of the numbers:",sub)
-    elif(operation=="multi"):
-        print("multipliation of the numbers:",multi)
-    elif(operation=="div"):
-        print("division of the numbers:",div)
-    else:
-        print("invalid operation")
+    try:
+        num1=int(input("Enter a number 1:"))
+        num2=int(input("Enter a number 2:"))
     
+        operation=input("Enter your operation[add/sub/multi/div/sqrt/power]:").lower()
+    
+        add=num1+num2
+        sub=num1-num2
+        multi=num1*num2
+        div = "undefined by zero" if num2 == 0 else num1 / num2
+        sqrt=math.sqrt
+        power=math.pow
+
+    
+        if (operation=="add"):
+            print("Addition of the numbers:", add)
+        elif(operation=="sub"):
+            print("Substraction of the numbers:",sub)
+        elif(operation=="multi"):
+            print("Multipliation of the numbers:",multi)
+        elif(operation=="div"): 
+            print(f"Division of the numbers:",div)
+        elif(operation=="sqrt"):
+            sqrt=math.sqrt(num1)
+            sqrt1=math.sqrt(num2)
+            print("square root of the given numbers:",sqrt ,sqrt1)
+        elif (operation=="power"):
+            power=math.pow(num1,num2)
+            print(f"{num1} raised to the power of {num2} is: {power}")
+        else:
+            print("invalid operation,Please enter a valid operation[add/sub/multi/div/sqrt/power]")
+    except ValueError:
+        print("Invalid input! Please enter numbers only.")
     choice = input("\nDo you want to perform another operation? (yes/no): ").lower()
     if choice!="yes":
        print("Exiting calculator...\nThank you for using calculator")
        break
-
-
-
-
-
-    
-        
-    
